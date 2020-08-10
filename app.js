1766a2092c4acb3c65089342a04e20b515d49e44
Raton Biswas <raton.biswas005@gmail.com>
2020-08-11 00:06:04 +0600

    user model|login route|using passport👍

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
+const passport = require('passport');
+const session = require('express-session');
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 // ** Load Config file
 dotenv.config({ path: './config/config.env'});
+
+// ** passport config 
+require('./config/passport')(passport);
+// require('./config/passport')(passportt)
 
 // ** Connect mongodb
 connectDB()
@@ -24,8 +32,27 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// ** session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+
+
+// ** passport middleware 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// static forlder 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // ** routes
 app.use('/',indexRouter);
+app.use('/auth',authRouter);
+
 
 const PORT = process.env.PORT || 3000;
 
