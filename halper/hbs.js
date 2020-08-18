8168b1d63303d244189b97f3c550d17636e75aa0
Raton Biswas <raton.biswas005@gmail.com>
2020-08-18 23:47:54 +0600

    Add edit function

--- halper/hbs.js
+++ halper/hbs.js
@@ -18,10 +18,10 @@ module.exports = {
     stripTags : function (input) {
         return input.replace(/^(?:.|\n)*?>/gm, '')
     },
-    editIcon : function (storyUser, loggedUser, storyId, floating = false){
+    editIcon : function (storyUser, loggedUser, storyId, floating = true){
         if (storyUser._id.toString() == loggedUser._id.toString()){
             if (floating){
-                return `<a href="/stories/edit/${storyId}" class="btn-floating halfwaf-fab blue"><i class="fas fa-edit fa-small"></i></a>`
+                return `<a href="/stories/edit/${storyId}" class="btn-floating halfwaf-fab blue right"><i class="fas fa-edit fa-small"></i></a>`
             }else{
                 return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>` 
             }
